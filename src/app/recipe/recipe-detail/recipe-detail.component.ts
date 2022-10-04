b6836c7bbe4b195f7b9829be46ac4d2b713ae584
Nishu-Gamage <nishuchatu@gmail.com>
2022-10-04 21:59:35 +0900

    2022/10/04

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,11 +6,19 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent implements OnInit, OnChanges {
 
   @Input() recipeItem:Recipe | undefined;
 
   constructor() { }
   ngOnInit(): void { }
+
+  ngOnChanges(change:SimpleChanges): void {
+    console.log(change);
+  }
+
+  ngDoCheck(){
+    console.log("ngDoCheck");
+  }
 
 }
--- src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, OnChanges,  SimpleChanges } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { Recipe } from '../../recipe.model';
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent implements OnInit, OnChanges{
 
   @Input() recipe:Recipe | undefined ;
   @Output() recipeSelected = new EventEmitter <void> ();
@@ -16,6 +16,10 @@ export class RecipeItemComponent implements OnInit {
 
   onSelect(){ 
     this.recipeSelected.emit();
+  }
+
+  ngOnChanges(change:SimpleChanges): void {
+    console.log(change);
   }
 
 }
